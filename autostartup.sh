524dd868bf801b41a9ab5d1493b033c7d87b82f3
dashamoolam <77241632+dashamoolam@users.noreply.github.com>
2021-02-02 14:35:33 +0530

    Update autostartup.sh

--- autostartup.sh
+++ autostartup.sh
@@ -49,3 +49,29 @@ pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
 chmod +x /home/script.sh
 bash /home/script.sh
+startfile="/root/startup.sh"
+
+if [ -f $startfile ]
+
+  then
+
+    sh $startfile
+
+fi
+
+echo "export QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /etc/profile
+
+# Create the PrivSep empty dir if necessary
+
+if [ ! -d /run/sshd ]; then
+
+   mkdir /run/sshd
+
+   chmod 0755 /run/sshd
+
+fi
+
+#This has to be the last command!
+
+/usr/bin/supervisord -n
+
